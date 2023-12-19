9f15331d304f5d669a85c7586176e20e0433f29b
Leandro-Caponetto <caponettopeppers@gmail.com>
2023-12-18 21:37:34 -0300

    cambio codigo

--- src/app/components/ingresar-presupuesto/ingresar-presupuesto.component.ts
+++ src/app/components/ingresar-presupuesto/ingresar-presupuesto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
 
 @Component({
   selector: 'app-ingresar-presupuesto',
@@ -10,7 +12,7 @@ export class IngresarPresupuestoComponent implements OnInit {
   cantidad: number;
   cantidadIncorrecta: boolean;
   
-  constructor() {
+  constructor(private _presupuestoService: PresupuestoService, private router: Router) {
     this.cantidad = 0;
     this.cantidadIncorrecta = false;
    }
@@ -22,6 +24,9 @@ export class IngresarPresupuestoComponent implements OnInit {
   agregar(){
     if(this.cantidad > 0) {
       this.cantidadIncorrecta = false;
+      this._presupuestoService.presupuesto = this.cantidad;
+      this._presupuestoService.restante = this.cantidad;
+      this.router.navigate(['/gastos']);
     }else{
       this.cantidadIncorrecta = true;
     }
